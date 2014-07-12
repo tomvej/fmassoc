0879539c378d56434d5105329db2b0ed2fe84d0b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-07-12 15:16:59 +0200

    rm #3472 dialog -- not yet sure how to propagate model and filter

---
+++ org.tomvej.fmassoc.e4toolbox/src/org/tomvej/fmassoc/e4toolbox/widgets/tablechooser/TableChooserDialog.java
@@ -0,0 +1,57 @@
+package org.tomvej.fmassoc.e4toolbox.widgets.tablechooser;
+
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.IMessageProvider;
+import org.eclipse.jface.dialogs.TitleAreaDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Shell;
+import org.tomvej.fmassoc.model.db.Table;
+
+public class TableChooserDialog extends TitleAreaDialog {
+	private TableChooser chooser;
+	private Table selected;
+
+	public TableChooserDialog(Shell parent) {
+		super(parent);
+	}
+
+	@Override
+	public void create() {
+		super.create();
+		setTitle("Select a Table");
+		setMessage("", IMessageProvider.INFORMATION);
+	}
+
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		chooser = new TableChooser(parent);
+		chooser.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+
+		chooser.setTableListener(table -> getButton(IDialogConstants.OK_ID)
+				.setEnabled(table != null));
+		return chooser;
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL,
+				true);
+		createButton(parent, IDialogConstants.CANCEL_ID,
+				IDialogConstants.CANCEL_LABEL, false);
+		getButton(IDialogConstants.OK_ID).setEnabled(false);
+	}
+
+	@Override
+	protected void okPressed() {
+		selected = chooser.getSelection();
+		super.okPressed();
+	}
+
+	public Table getSelectedTable() {
+		return selected;
+	}
+
+}
