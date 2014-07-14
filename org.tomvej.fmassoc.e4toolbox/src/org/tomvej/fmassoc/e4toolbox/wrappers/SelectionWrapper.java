0e90d574d9a82d1e0d3c1e4c1c4ca6081959dcb4
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-07-14 14:39:21 +0200

    Wrapper for SelectionListener.

---
+++ org.tomvej.fmassoc.e4toolbox/src/org/tomvej/fmassoc/e4toolbox/wrappers/SelectionWrapper.java
@@ -0,0 +1,33 @@
+package org.tomvej.fmassoc.e4toolbox.wrappers;
+
+import java.util.function.Consumer;
+
+import org.apache.commons.lang3.Validate;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+/**
+ * 
+ * Wrapper of {@link SelectionListener} so that it can be used functionally.
+ * Uses {@link SelectionListener#widgetSelected(SelectionEvent)} method.
+ * 
+ * @author Tomáš Vejpustek
+ *
+ */
+public class SelectionWrapper extends SelectionAdapter {
+	private final Consumer<SelectionEvent> listener;
+	
+	/**
+	 * Specify action for {@link SelectionListener#widgetSelected(SelectionEvent)}.
+	 * @param listener
+	 */
+	public SelectionWrapper(Consumer<SelectionEvent> listener) {
+		this.listener = Validate.notNull(listener);
+	}
+	
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+		listener.accept(e);
+	}
+}
