480404b63464558f387ecf4784b01553ecc8b87c
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-07-22 09:21:02 +0200

    New wrapper for widgetDefaultSelected

---
+++ org.tomvej.fmassoc.e4toolbox/src/org/tomvej/fmassoc/e4toolbox/wrappers/DefaultSelectionWrapper.java
@@ -0,0 +1,34 @@
+package org.tomvej.fmassoc.e4toolbox.wrappers;
+
+import java.util.function.Consumer;
+
+import org.apache.commons.lang3.Validate;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
+
+/**
+ * 
+ * Wrapper of {@link SelectionListener} so that it can be used functionally.
+ * Uses {@link SelectionListener#widgetDefaultSelected(SelectionEvent)} method.
+ * 
+ * @author Tomáš Vejpustek
+ *
+ */
+public class DefaultSelectionWrapper extends SelectionAdapter {
+	private final Consumer<SelectionEvent> listener;
+	
+	/**
+	 * Specify action for {@link SelectionListener#widgetDefaultSelected(SelectionEvent)}.
+	 * @param listener
+	 * 		Performed for {@link SelectionListener#widgetDefaultSelected(SelectionEvent)}
+	 */
+	public DefaultSelectionWrapper(Consumer<SelectionEvent> listener) {
+		this.listener = Validate.notNull(listener);
+	}
+
+	@Override
+	public void widgetDefaultSelected(SelectionEvent e) {
+		listener.accept(e);
+	}
+}
