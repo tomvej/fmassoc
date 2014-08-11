131d0c9ab9a4c650c0156a40123e2b57430c54df
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-08-11 14:26:35 +0200

    rm #3473 Found Paths stub

---
+++ org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/Part.java
@@ -0,0 +1,28 @@
+package org.tomvej.fmassoc.parts.paths;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.jface.viewers.TableViewerColumn;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+
+public class Part {
+	private TableViewer pathTable;
+
+	@PostConstruct
+	public void createComponents(Composite parent, ESelectionService selectionService) {
+		pathTable = new TableViewer(parent, SWT.SINGLE | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
+		pathTable.getTable().setHeaderVisible(true);
+		pathTable.getTable().setLinesVisible(true);
+
+		TableViewerColumn pathColumn = new TableViewerColumn(pathTable, SWT.LEFT);
+		pathColumn.getColumn().setText("Path");
+
+		pathTable.addSelectionChangedListener(e -> selectionService.setSelection(
+				((IStructuredSelection) pathTable.getSelection()).getFirstElement()));
+	}
+
+}
