76e8199adb3473c5a9cbcd7734551079d6ba1f57
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-08-12 22:24:03 +0200

    rm #3473 default label provider reflects skeletal implementation

--- plugins/org.tomvej.fmassoc.plugin.pathlabelprovider/src/org/tomvej/fmassoc/plugin/pathlabelprovider/PathLabelProvider.java
+++ plugins/org.tomvej.fmassoc.plugin.pathlabelprovider/src/org/tomvej/fmassoc/plugin/pathlabelprovider/PathLabelProvider.java
@@ -1,5 +1,30 @@
 package org.tomvej.fmassoc.plugin.pathlabelprovider;
 
-import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.tomvej.fmassoc.model.db.AssociationProperty;
+import org.tomvej.fmassoc.model.db.Table;
+import org.tomvej.fmassoc.parts.paths.labelprovider.TextPathLabelProvider;
 
-public class PathLabelProvider extends ColumnLabelProvider {}
+public class PathLabelProvider extends TextPathLabelProvider {
+
+	@Override
+	protected String getTableText(Table target) {
+		return target.getImplName();
+	}
+
+	@Override
+	protected String getAssociationText(AssociationProperty target) {
+		// TODO add formatter
+		return " " + " ";
+	}
+
+	@Override
+	protected String getTableToolTipText(Table target) {
+		return target.getImplName() + " (" + target.getName() + ")\n";
+	}
+
+	@Override
+	protected String getAssociationToolTipText(AssociationProperty target) {
+		// TODO add formatter
+		return "   " + " " + target.getName() + "\n";
+	}
+}
