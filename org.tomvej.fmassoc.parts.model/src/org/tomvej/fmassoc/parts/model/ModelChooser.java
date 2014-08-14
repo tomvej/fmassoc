6e17a42ee8df9fcfeb4ede08d119b6acbd77fc44
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-08-14 09:05:13 +0200

    rm #3713 added model loader plugin and toolbar

---
+++ org.tomvej.fmassoc.parts.model/src/org/tomvej/fmassoc/parts/model/ModelChooser.java
@@ -0,0 +1,14 @@
+package org.tomvej.fmassoc.parts.model;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class ModelChooser {
+
+	@PostConstruct
+	public void createComponents(Composite parent) {
+		// TODO
+	}
+
+}
