081d55837cd72e8996270cb6c38c5fc2ee32bda9
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-08-18 21:28:25 +0200

    rm #3713 constant for model loader registry

--- org.tomvej.fmassoc.parts.model/src/org/tomvej/fmassoc/parts/model/core/Constants.java
+++ org.tomvej.fmassoc.parts.model/src/org/tomvej/fmassoc/parts/model/core/Constants.java
@@ -1,5 +1,6 @@
 package org.tomvej.fmassoc.parts.model.core;
 
+
 /**
  * Constants used by the org.tomvej.fmassoc.parts.model plugin.
  * 
@@ -11,4 +12,10 @@ public interface Constants {
 	 * Id of this plugin.
 	 */
 	public static final String PLUGIN_ID = "org.tomvej.fmassoc.parts.model";
+
+	/**
+	 * Constant for storing list of model loaders in the eclipse context.
+	 * Relates to {@code List<ModelLoaderEntry>}.
+	 */
+	public static final String MODEL_LOADER_REGISTRY = PLUGIN_ID + ".ModelLoaderRegistry";
 }
