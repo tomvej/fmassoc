d12b8d9f9820389cd656145e02d460fb53417fdc
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-08-27 20:40:51 +0200

    rm #3473 found paths table corrected to show at least something

--- org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/Part.java
+++ org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/Part.java
@@ -6,6 +6,7 @@ import javax.inject.Inject;
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.UIEventTopic;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.layout.GridDataFactory;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.TableViewerColumn;
@@ -32,12 +33,14 @@ public class Part {
 		pathTable = new TableViewer(parent, SWT.SINGLE | SWT.FULL_SELECTION | SWT.V_SCROLL | SWT.H_SCROLL);
 		pathTable.getTable().setHeaderVisible(true);
 		pathTable.getTable().setLinesVisible(true);
+		pathTable.getTable().setLayoutData(GridDataFactory.fillDefaults().grab(true, true).create());
 
 		TableViewerColumn pathColumn = new TableViewerColumn(pathTable, SWT.LEFT);
 		pathColumn.getColumn().setText("Path");
+		pathColumn.getColumn().setWidth(100);
 
 		// FIXME create different action provider
-		pathColumn.setLabelProvider(new TextColumnLabelProvider<Path>(path -> path.toString()));
+		pathColumn.setLabelProvider(new TextColumnLabelProvider<Path>(path -> path.getAssociations().toString()));
 
 		pathTable.addSelectionChangedListener(e -> selectionService.setSelection(
 				((IStructuredSelection) pathTable.getSelection()).getFirstElement()));
