b7e28c19acf818827fb983ecef420e7e5b7cc99c
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-08-29 10:04:35 +0200

    rm #3777 new lambda-enabled pruning

--- plugins/org.tomvej.fmassoc.plugin.prioritydfpathfinder/src/org/tomvej/fmassoc/plugin/prioritydfpathfinder/AbstractPruning.java
+++ plugins/org.tomvej.fmassoc.plugin.prioritydfpathfinder/src/org/tomvej/fmassoc/plugin/prioritydfpathfinder/AbstractPruning.java
@@ -1,9 +1,12 @@
 package org.tomvej.fmassoc.plugin.prioritydfpathfinder;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.apache.commons.lang3.Validate;
 import org.tomvej.fmassoc.model.property.PathProperty;
 
 /**
@@ -18,8 +21,23 @@ public abstract class AbstractPruning implements Pruning {
 	/**
 	 * Specify used properties.
 	 */
+	public AbstractPruning(PathProperty<?> property) {
+		this.properties = Collections.singleton(Validate.notNull(property));
+	}
+
+	/**
+	 * Specify used properties.
+	 */
+	public AbstractPruning(Collection<PathProperty<?>> properties) {
+		this.properties = Collections.unmodifiableSet(new HashSet<>(properties));
+		Validate.isTrue(!this.properties.contains(null), "There is null element inside supplied properties.");
+	}
+
+	/**
+	 * Specify used properties.
+	 */
 	public AbstractPruning(PathProperty<?>... properties) {
-		this.properties = new HashSet<>(Arrays.asList(properties));
+		this(Arrays.asList(properties));
 	}
 
 	@Override
