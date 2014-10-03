5b3be6d88c7e2907159d0dc8dadfcfa7ef9dbe74
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-10-03 15:33:18 +0200

    rm #3968 basic plugin structure

---
+++ plugins/org.tomvej.fmassoc.plugin.simplepruningfinder/src/org/tomvej/fmassoc/plugin/simplepruningfinder/Part.java
@@ -0,0 +1,13 @@
+package org.tomvej.fmassoc.plugin.simplepruningfinder;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Part {
+
+	@PostConstruct
+	public void createComponents(Composite parent) {
+
+	}
+}
