db805cc2717ff79d83debbceafb484c061e283be
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-10-06 10:42:30 +0200

    rm #3968 iterable pruning building

--- plugins/org.tomvej.fmassoc.plugin.simplepruningfinder/src/org/tomvej/fmassoc/plugin/simplepruningfinder/SimplePathFinderProvider.java
+++ plugins/org.tomvej.fmassoc.plugin.simplepruningfinder/src/org/tomvej/fmassoc/plugin/simplepruningfinder/SimplePathFinderProvider.java
@@ -1,6 +1,6 @@
 package org.tomvej.fmassoc.plugin.simplepruningfinder;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.tomvej.fmassoc.plugin.prioritydfpathfinder.IteratedPriorityDFFinderProvider;
@@ -21,8 +21,27 @@ public class SimplePathFinderProvider extends IteratedPriorityDFFinderProvider {
 	}
 
 	private static List<Pruning> generatePruning(Settings settings) {
-		// TODO
-		return Collections.emptyList();
+		List<Pruning> result = new ArrayList<>();
+		for (int width = 0; width <= settings.getWidthLimit(); width++) {
+			for (int length = 3; length < settings.getLengthLimit(); length += 3) {
+				addPruningsForLength(result, width, length, settings.searchOptional(), settings.searchMN());
+			}
+			addPruningsForLength(result, width, settings.getLengthLimit(), settings.searchOptional(), settings.searchMN());
+		}
+		return result;
+	}
+
+	private static void addPruningsForLength(List<Pruning> result, int width, int length, boolean optional, boolean nm) {
+		result.add(new SimplePruning(new Settings(false, false, length, width)));
+		if (optional) {
+			result.add(new SimplePruning(new Settings(true, false, length, width)));
+		}
+		if (nm) {
+			result.add(new SimplePruning(new Settings(false, true, length, width)));
+			if (optional) {
+				result.add(new SimplePruning(new Settings(true, true, length, width)));
+			}
+		}
 	}
 
 }
