f82252c206097b8946a4353115811e5f895f0927
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-10-10 15:36:04 +0200

    rm #3777 concurrent search job prevented

--- org.tomvej.fmassoc.core/src/org/tomvej/fmassoc/core/search/internal/SearchPaths.java
+++ org.tomvej.fmassoc.core/src/org/tomvej/fmassoc/core/search/internal/SearchPaths.java
@@ -6,10 +6,12 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.e4.core.contexts.IEclipseContext;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.core.di.extensions.EventTopic;
 import org.eclipse.e4.core.services.events.IEventBroker;
 import org.eclipse.e4.core.services.log.Logger;
 import org.tomvej.fmassoc.core.communicate.ContextObjects;
@@ -21,6 +23,12 @@ import org.tomvej.fmassoc.model.path.Path;
 public class SearchPaths {
 	private final List<Path> foundPaths;
 
+	// the context must be shared -- otherwise, the job might not be removed
+	// from the right context
+	@Inject
+	private IEclipseContext context;
+
+
 	@Inject
 	public SearchPaths(IEclipseContext context) {
 		foundPaths = new ArrayList<>();
@@ -28,8 +36,7 @@ public class SearchPaths {
 	}
 
 	@Execute
-	public void execute(SearchInput input, PathFinderProvider provider, IEclipseContext context, IEventBroker broker,
-			Logger logger) {
+	public void execute(SearchInput input, PathFinderProvider provider, IEventBroker broker, Logger logger) {
 		PathFinderJob job = new PathFinderJob(provider.createPathFinder(input), broker, foundPaths, logger);
 		context.set(PathFinderJob.class, job);
 		foundPaths.clear();
@@ -41,7 +48,18 @@ public class SearchPaths {
 
 	@Optional
 	@CanExecute
-	public boolean canExecute(SearchInput input, PathFinderProvider provider) {
-		return input != null && provider != null;
+	public boolean canExecute(SearchInput input, PathFinderProvider provider, PathFinderJob job) {
+		return input != null && provider != null && job == null;
 	}
+
+	@Inject
+	public void jobFinished(@Optional @EventTopic(PathSearchTopic.FINISH) IStatus status) {
+		context.set(PathFinderJob.class, null);
+	}
+
+	@Inject
+	public void jobCancelled(@Optional @EventTopic(PathSearchTopic.CANCEL) IStatus status) {
+		context.set(PathFinderJob.class, null);
+	}
+
 }
--- org.tomvej.fmassoc.core/src/org/tomvej/fmassoc/core/search/internal/StopSearch.java
+++ org.tomvej.fmassoc.core/src/org/tomvej/fmassoc/core/search/internal/StopSearch.java
@@ -1,21 +1,23 @@
 package org.tomvej.fmassoc.core.search.internal;
 
-import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.e4.core.di.annotations.CanExecute;
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.tomvej.fmassoc.core.communicate.PathSearchTopic;
 
 public class StopSearch {
 
 	@Execute
-	public void execute(PathFinderJob currentJob, IEclipseContext context) {
+	public void execute(PathFinderJob currentJob, IEventBroker broker) {
 		if (currentJob.cancel()) {
-			context.remove(PathFinderJob.class);
+			// the job has not started yet, so it will not send cancel
+			broker.post(PathSearchTopic.CANCEL, Status.CANCEL_STATUS);
 		}
 	}
 
 	@CanExecute
 	public boolean canExecute(PathFinderJob currentJob) {
-		// some other check? is not already cancelled?
 		return currentJob != null;
 	}
 
