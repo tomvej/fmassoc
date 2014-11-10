4482e5a3e5ce598e517c293e5dcbde2eba8efbb7
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2014-11-10 16:54:56 +0100

    rm #3777 simple dialog which opens when searching

---
+++ org.tomvej.fmassoc.core/src/org/tomvej/fmassoc/core/search/internal/SearchProgressDialog.java
@@ -0,0 +1,50 @@
+package org.tomvej.fmassoc.core.search.internal;
+
+import javax.inject.Inject;
+
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.di.UIEventTopic;
+import org.eclipse.e4.ui.model.application.ui.basic.MDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.ProgressBar;
+import org.tomvej.fmassoc.core.communicate.PathSearchTopic;
+import org.tomvej.fmassoc.core.search.SearchInput;
+
+public class SearchProgressDialog {
+
+	@Optional
+	@Inject
+	public void createControls(Composite parent) {
+		new ProgressBar(parent, SWT.HORIZONTAL | SWT.SMOOTH | SWT.INDETERMINATE);
+
+		Button alwaysBgBtn = new Button(parent, SWT.CHECK);
+		alwaysBgBtn.setText("Always run in background.");
+
+		Button bgBtn = new Button(parent, SWT.PUSH);
+		bgBtn.setText("Run in background");
+
+		Button cancelBtn = new Button(parent, SWT.PUSH);
+		cancelBtn.setText("Cancel");
+	}
+
+	@Optional
+	@Inject
+	public void searchStarted(@UIEventTopic(PathSearchTopic.START) SearchInput input, MDialog dialog) {
+		dialog.setVisible(true);
+	}
+
+	@Optional
+	@Inject
+	public void searchFinished(@UIEventTopic(PathSearchTopic.FINISH) IStatus status, MDialog dialog) {
+		dialog.setVisible(false);
+	}
+
+	@Optional
+	@Inject
+	public void searchCancelled(@UIEventTopic(PathSearchTopic.CANCEL) IStatus status, MDialog dialog) {
+		dialog.setVisible(false);
+	}
+}
