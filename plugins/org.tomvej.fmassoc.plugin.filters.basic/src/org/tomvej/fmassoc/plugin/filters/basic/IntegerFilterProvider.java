217a303c3c750acfa2ccdc552d8687952be2f87e
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-02-06 10:06:45 +0100

    rm #3812 general operators for Objects and Comparables

--- plugins/org.tomvej.fmassoc.plugin.filters.basic/src/org/tomvej/fmassoc/plugin/filters/basic/IntegerFilterProvider.java
+++ plugins/org.tomvej.fmassoc.plugin.filters.basic/src/org/tomvej/fmassoc/plugin/filters/basic/IntegerFilterProvider.java
@@ -1,7 +1,8 @@
 package org.tomvej.fmassoc.plugin.filters.basic;
 
-import java.util.Collections;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.tomvej.fmassoc.parts.paths.filterprovider.Filter;
 import org.tomvej.fmassoc.parts.paths.filterprovider.FilterProvider;
@@ -16,16 +17,9 @@ public class IntegerFilterProvider implements FilterProvider<Integer> {
 
 	@Override
 	public Filter<Integer> get() {
-		return new OperatorFilter<>(Collections.singletonList(new Operator<Integer>() {
-			@Override
-			public boolean test(Integer t1, Integer t2) {
-				return Objects.equals(t1, t2);
-			}
-
-			@Override
-			public String toString() {
-				return "=";
-			}
-		}), new SpinnerProvider());
+		Collection<Operator<? super Integer>> operators = new ArrayList<>();
+		operators.addAll(Arrays.asList(ObjectOperator.values()));
+		operators.addAll(Arrays.asList(ComparableOperator.values()));
+		return new OperatorFilter<>(operators, new SpinnerProvider());
 	}
 }
---
+++ plugins/org.tomvej.fmassoc.plugin.filters.basic/src/org/tomvej/fmassoc/plugin/filters/basic/ObjectOperator.java
@@ -0,0 +1,36 @@
+package org.tomvej.fmassoc.plugin.filters.basic;
+
+import java.util.Objects;
+
+/**
+ * Operators applicable on all objects.
+ * 
+ * @author Tomáš Vejpustek
+ */
+public enum ObjectOperator implements Operator<Object> {
+	/** Equality operator */
+	EQ("=") {
+		@Override
+		public boolean test(Object t1, Object t2) {
+			return Objects.equals(t1, t2);
+		}
+	},
+	/** non-equality operator */
+	NEQ("!=") {
+		@Override
+		public boolean test(Object t1, Object t2) {
+			return !Objects.equals(t1, t2);
+		}
+	};
+
+	private final String code;
+
+	private ObjectOperator(String code) {
+		this.code = code;
+	}
+
+	@Override
+	public String toString() {
+		return code;
+	}
+}
