9370d003a6db5271c9b636e8448a7cf1fdfebd95
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-02-06 08:43:01 +0100

    rm #3812 refactoring

---
+++ plugins/org.tomvej.fmassoc.plugin.filters.basic/src/org/tomvej/fmassoc/plugin/filters/basic/SpinnerProvider.java
@@ -0,0 +1,28 @@
+package org.tomvej.fmassoc.plugin.filters.basic;
+
+import java.util.function.Consumer;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Spinner;
+
+/**
+ * Creates spinner to provide {@link Integer} values.
+ * 
+ * @author Tomáš Vejpustek
+ */
+final class SpinnerProvider implements ValueControlProvider<Integer> {
+
+	@Override
+	public Control createControl(Composite parent, Consumer<Integer> listener, Integer initial) {
+		Spinner input = new Spinner(parent, SWT.BORDER);
+		if (initial == null) {
+			initial = 0;
+			listener.accept(0);
+		}
+		input.setValues(initial, Integer.MIN_VALUE, Integer.MAX_VALUE, 0, 1, 10);
+		input.addModifyListener(e -> listener.accept(input.getSelection()));
+		return input;
+	}
+}
