1fae5bee6e806ef37503e06652bf848358f10633
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-02-17 13:34:02 +0100

    rm #3969 drag and drop

--- plugins/org.tomvej.fmassoc.plugin.filterpruningfinder/src/org/tomvej/fmassoc/plugin/filterpruningfinder/Part.java
+++ plugins/org.tomvej.fmassoc.plugin.filterpruningfinder/src/org/tomvej/fmassoc/plugin/filterpruningfinder/Part.java
@@ -1,11 +1,89 @@
 package org.tomvej.fmassoc.plugin.filterpruningfinder;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.annotation.PostConstruct;
+import javax.inject.Named;
+
+import org.eclipse.e4.core.services.log.Logger;
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.tomvej.fmassoc.core.communicate.ContextObjects;
+import org.tomvej.fmassoc.core.dnd.CompositeDnDSupport;
+import org.tomvej.fmassoc.core.properties.PathPropertyEntry;
+import org.tomvej.fmassoc.core.wrappers.SelectionWrapper;
+import org.tomvej.fmassoc.filter.FilterProvider;
+import org.tomvej.fmassoc.filter.FilterRegistry;
 
 public class Part {
+	private Map<PathPropertyEntry<?>, FilterProvider<?>> providers;;
+
+	private Composite pruningPanel;
+	private PruningRow basePruning;
+	private List<PruningRow> pruning;
+	private CompositeDnDSupport dndSupport;
 
 	@PostConstruct
-	public void createComponents() {
+	public void createComponents(Composite parent, FilterRegistry filters, Logger logger,
+			@Named(ContextObjects.PATH_PROPERTIES) List<PathPropertyEntry<?>> pathProperties) {
+		// generate provider map
+		providers = new HashMap<>();
+		for (PathPropertyEntry<?> property : pathProperties) {
+			FilterProvider<?> provider = filters.apply(property.getProperty().getType());
+			if (provider != null) {
+				providers.put(property, provider);
+			} else {
+				logger.warn("No filter for " + property.getName() + ".");
+			}
+		}
+		providers = Collections.unmodifiableMap(providers);
 
+		// generate component
+		parent.setLayout(new GridLayout(2, false));
+
+		GridDataFactory pruningLayout = GridDataFactory.fillDefaults().span(2, 1).grab(true, false);
+		basePruning = new PruningRow(parent, providers, false);
+		basePruning.setLayoutData(pruningLayout.create());
+
+		pruningPanel = new Composite(parent, SWT.NONE);
+		pruningPanel.setLayoutData(pruningLayout.create());
+		GridLayout pruningPanelLayout = new GridLayout();
+		pruningPanelLayout.marginWidth = 0;
+		pruningPanelLayout.marginHeight = 0;
+		pruningPanel.setLayout(pruningPanelLayout);
+
+		Button addBtn = new Button(parent, SWT.PUSH);
+		addBtn.setLayoutData(GridDataFactory.fillDefaults().align(SWT.BEGINNING, SWT.FILL).create());
+		addBtn.setText("Add");
+		addBtn.addSelectionListener(new SelectionWrapper(e -> addRow()));
+
+		Button clearBtn = new Button(parent, SWT.PUSH);
+		clearBtn.setLayoutData(GridDataFactory.fillDefaults().align(SWT.END, SWT.FILL).create());
+		clearBtn.setText("Remove All");
+		clearBtn.addSelectionListener(new SelectionWrapper(e -> clearRows()));
+
+		// initialize reordering
+		dndSupport = new CompositeDnDSupport(pruningPanel);
+		pruning = new ArrayList<>();
 	}
+
+	private void addRow() {
+		PruningRow newRow = new PruningRow(pruningPanel, providers, true);
+		pruning.add(newRow);
+		newRow.pluginDnD(dndSupport);
+		pruningPanel.getParent().layout();
+	}
+
+	private void clearRows() {
+		pruning.forEach(p -> p.dispose());
+		pruningPanel.getParent().layout();
+	}
+
 }
---
+++ plugins/org.tomvej.fmassoc.plugin.filterpruningfinder/src/org/tomvej/fmassoc/plugin/filterpruningfinder/PruningRow.java
@@ -0,0 +1,70 @@
+package org.tomvej.fmassoc.plugin.filterpruningfinder;
+
+import java.util.Map;
+import java.util.function.Predicate;
+
+import org.eclipse.jface.layout.GridDataFactory;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.tomvej.fmassoc.core.dnd.CompositeDnDSupport;
+import org.tomvej.fmassoc.core.properties.PathPropertyEntry;
+import org.tomvej.fmassoc.core.wrappers.SelectionWrapper;
+import org.tomvej.fmassoc.filter.FilterProvider;
+import org.tomvej.fmassoc.filter.dialog.FilterDialog;
+import org.tomvej.fmassoc.model.path.Path;
+
+public class PruningRow extends Composite {
+	private FilterDialog dialog;
+	private Label filterLbl;
+
+	public PruningRow(Composite parent, Map<PathPropertyEntry<?>, FilterProvider<?>> providers, boolean removable) {
+		super(parent, SWT.BORDER);
+		GridDataFactory grabLayout = GridDataFactory.fillDefaults().grab(true, false);
+		setLayoutData(grabLayout.create());
+		setLayout(new GridLayout(3, false));
+
+		dialog = new FilterDialog(getShell());
+		dialog.setColumns(providers);
+
+		filterLbl = new Label(this, SWT.WRAP);
+		filterLbl.setLayoutData(grabLayout.create());
+
+		GridDataFactory btnLayout = GridDataFactory.fillDefaults().align(SWT.FILL, SWT.TOP);
+
+		Button filterBtn = new Button(this, SWT.PUSH);
+		filterBtn.setText("...");
+		filterBtn.setLayoutData(btnLayout.create());
+		filterBtn.addSelectionListener(new SelectionWrapper(e -> showFilter()));
+
+		if (removable) {
+			Button rmBtn = new Button(this, SWT.PUSH);
+			rmBtn.setText("X");
+			rmBtn.setLayoutData(btnLayout.create());
+			rmBtn.addSelectionListener(new SelectionWrapper(e -> {
+				dispose();
+				parent.getParent().layout();
+			}));
+		}
+	}
+
+	private void showFilter() {
+		dialog.open();
+		filterLbl.setText(dialog.getFilter() != null ? dialog.getFilter().toString() : "");
+		getParent().layout(); // this one is for base pruning
+		getParent().getParent().layout();
+	}
+
+	public Predicate<Path> getPruning() {
+		return dialog.getFilter();
+	}
+
+	public void pluginDnD(CompositeDnDSupport support) {
+		support.registerKnob(filterLbl, this);
+		support.registerKnob(this, this);
+	}
+
+
+}
