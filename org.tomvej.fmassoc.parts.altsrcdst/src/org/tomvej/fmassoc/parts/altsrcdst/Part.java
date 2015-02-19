fdb076d73bac471c56f445629e659dda00f2640b
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-02-19 15:38:21 +0100

    rm #4047 plugin initiation

---
+++ org.tomvej.fmassoc.parts.altsrcdst/src/org/tomvej/fmassoc/parts/altsrcdst/Part.java
@@ -0,0 +1,13 @@
+package org.tomvej.fmassoc.parts.altsrcdst;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Part {
+
+	@PostConstruct
+	public void createComponents(Composite parent) {
+
+	}
+}
