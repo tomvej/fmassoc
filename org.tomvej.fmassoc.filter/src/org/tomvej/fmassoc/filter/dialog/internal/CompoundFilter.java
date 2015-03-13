d0d96040075cafc466e5622c59c1e697e0d18e77
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-03-13 11:09:13 +0100

    rm #4969 Compound filter toString

--- org.tomvej.fmassoc.filter/src/org/tomvej/fmassoc/filter/dialog/internal/CompoundFilter.java
+++ org.tomvej.fmassoc.filter/src/org/tomvej/fmassoc/filter/dialog/internal/CompoundFilter.java
@@ -3,6 +3,7 @@ package org.tomvej.fmassoc.filter.dialog.internal;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.Validate;
 
@@ -29,13 +30,7 @@ public class CompoundFilter<T> implements Predicate<T> {
 
 	@Override
 	public String toString() {
-		StringBuilder result = new StringBuilder();
-		filters.forEach(e -> result.append(e).append(", "));
-		int length = result.length();
-		if (length > 0) {
-			result.delete(length - 2, length);
-		}
-		return result.toString();
+		return filters.stream().map(Object::toString).collect(Collectors.joining(", "));
 	}
 
 }
