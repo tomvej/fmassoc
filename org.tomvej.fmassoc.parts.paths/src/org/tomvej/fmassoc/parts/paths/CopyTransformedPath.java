ed1f288ee972e25eeb93dea84f3b6c8dadf2f496
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-04-14 13:27:34 +0200

    rm #5100 javadoc

--- org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/CopyTransformedPath.java
+++ org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/CopyTransformedPath.java
@@ -11,19 +11,35 @@ import org.eclipse.swt.dnd.Transfer;
 import org.eclipse.swt.widgets.Display;
 import org.tomvej.fmassoc.core.communicate.ContextObjects;
 
+/**
+ * Copies transformed path into clipboard.
+ * 
+ * @author Tomáš Vejpustek
+ */
 public class CopyTransformedPath {
 	private Clipboard clipboard;
 
+	/**
+	 * Initialize clipboard.
+	 * 
+	 * @param display
+	 */
 	@PostConstruct
 	public void initializeClipboard(Display display) {
 		clipboard = new Clipboard(display);
 	}
 
+	/**
+	 * Copy selected path into clipboard.
+	 */
 	@Execute
 	public void execute(@Named(ContextObjects.TRANSFORMED_PATH) String path) {
 		clipboard.setContents(new Object[] { path }, new Transfer[] { TextTransfer.getInstance() });
 	}
 
+	/**
+	 * Release clipboard system resource.
+	 */
 	@PreDestroy
 	public void disposeClipboard() {
 		clipboard.dispose();
