9a736786de78e7e96443f16be4039987fa7d0b3d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-04-14 13:26:13 +0200

    rm #5100 clean-up

--- org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/CopyTransformedPath.java
+++ org.tomvej.fmassoc.parts.paths/src/org/tomvej/fmassoc/parts/paths/CopyTransformedPath.java
@@ -2,11 +2,9 @@ package org.tomvej.fmassoc.parts.paths;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Execute;
-import org.eclipse.e4.core.services.log.Logger;
 import org.eclipse.swt.dnd.Clipboard;
 import org.eclipse.swt.dnd.TextTransfer;
 import org.eclipse.swt.dnd.Transfer;
@@ -14,8 +12,6 @@ import org.eclipse.swt.widgets.Display;
 import org.tomvej.fmassoc.core.communicate.ContextObjects;
 
 public class CopyTransformedPath {
-	@Inject
-	private Logger logger;
 	private Clipboard clipboard;
 
 	@PostConstruct
@@ -25,7 +21,6 @@ public class CopyTransformedPath {
 
 	@Execute
 	public void execute(@Named(ContextObjects.TRANSFORMED_PATH) String path) {
-		logger.info("Transformed path copied.");
 		clipboard.setContents(new Object[] { path }, new Transfer[] { TextTransfer.getInstance() });
 	}
 
