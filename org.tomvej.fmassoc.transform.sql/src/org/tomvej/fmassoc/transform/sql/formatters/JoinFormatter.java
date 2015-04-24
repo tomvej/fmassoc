e9794b052535808b8385f93fdba8d29fc7a5c364
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-04-24 13:00:09 +0200

    rm #5123 formatter can now use left join

--- org.tomvej.fmassoc.transform.sql/src/org/tomvej/fmassoc/transform/sql/formatters/JoinFormatter.java
+++ org.tomvej.fmassoc.transform.sql/src/org/tomvej/fmassoc/transform/sql/formatters/JoinFormatter.java
@@ -15,17 +15,35 @@ import org.tomvej.fmassoc.transform.sql.handles.HandleFactory;
 public class JoinFormatter {
 	private final HandleFactory factory;
 	private final boolean allColumns;
+	private final boolean leftJoin;
 
 	/**
 	 * Specify handle factory.
 	 * 
 	 * @param displayAllColumns
 	 *            when {@code true}, display settings of individual handles are
-	 *            suppressed and all columns are displayed (SELECT * FROM)
+	 *            suppressed and all columns are displayed ({@code SELECT * FROM}
+	 *            )
 	 */
 	public JoinFormatter(HandleFactory handleFactory, boolean displayAllColumns) {
+		this(handleFactory, displayAllColumns, false);
+	}
+
+	/**
+	 * Specify handle factory.
+	 * 
+	 * @param displayAllColumns
+	 *            when {@code true}, display settings of individual
+	 *            handlers are suppressed and all columns are displayed
+	 *            (as in {@code SELECT * FROM}).
+	 * @param useLeftJoin
+	 *            when {@code true} use {@code LEFT JOIN} instead of
+	 *            {@code JOIN}
+	 */
+	public JoinFormatter(HandleFactory handleFactory, boolean displayAllColumns, boolean useLeftJoin) {
 		factory = Validate.notNull(handleFactory);
 		allColumns = displayAllColumns;
+		leftJoin = useLeftJoin;
 	}
 
 	/**
@@ -48,6 +66,9 @@ public class JoinFormatter {
 		result.append(" FROM ");
 		result.append(factory.getTableHandle(target.getSource()).getDeclaration());
 		for (AssociationProperty assoc : target.getAssociations()) {
+			if (leftJoin) {
+				result.append(" LEFT");
+			}
 			result.append(" JOIN ");
 			result.append(factory.getTableHandle(assoc.getDestination()).getDeclaration());
 			result.append(" ON ");
