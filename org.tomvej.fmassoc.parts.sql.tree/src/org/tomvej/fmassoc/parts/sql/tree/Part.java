62e3a6e586c0d428b16b4cc1f80f1b3cb68f87b5
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-04-29 09:42:11 +0200

    rm #5186 plugin introduction

---
+++ org.tomvej.fmassoc.parts.sql.tree/src/org/tomvej/fmassoc/parts/sql/tree/Part.java
@@ -0,0 +1,13 @@
+package org.tomvej.fmassoc.parts.sql.tree;
+
+import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.widgets.Composite;
+
+public class Part {
+
+	@PostConstruct
+	public void createComponents(Composite parent) {
+
+	}
+}
