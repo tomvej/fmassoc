9733c3f2264ad2fa792aeebcf0d53cba8b5b0a5d
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-05-19 10:49:19 +0200

    rm #5186 better label provider

--- org.tomvej.fmassoc.parts.sql.tree/src/org/tomvej/fmassoc/parts/sql/tree/PathTreeLabelProvider.java
+++ org.tomvej.fmassoc.parts.sql.tree/src/org/tomvej/fmassoc/parts/sql/tree/PathTreeLabelProvider.java
@@ -1,6 +1,7 @@
 package org.tomvej.fmassoc.parts.sql.tree;
 
 import org.eclipse.jface.viewers.ColumnLabelProvider;
+import org.tomvej.fmassoc.model.db.AssociationProperty;
 import org.tomvej.fmassoc.model.db.Property;
 import org.tomvej.fmassoc.model.db.Table;
 import org.tomvej.fmassoc.parts.sql.tree.model.AssociationColumns;
@@ -13,18 +14,27 @@ public class PathTreeLabelProvider extends ColumnLabelProvider {
 	public String getText(Object element) {
 		if (element instanceof Table) {
 			return ((Table) element).getName() + " (" + ((Table) element).getImplName() + ")";
+
+		} else if (element instanceof AssociationProperty) {
+			AssociationProperty property = (AssociationProperty) element;
+			return property.getName() + " " + property.getOther().getName();
+
 		} else if (element instanceof Property) {
 			return ((Property) element).getName();
+
 		} else if (element instanceof ObjectIdColumn) {
 			return ((ObjectIdColumn) element).getParent().getIDImplName();
+
 		} else if (element instanceof AssociationColumns) {
 			return "associations";
+
 		} else if (element instanceof PropertyColumns) {
 			return "properties";
+
 		} else if (element instanceof VersionColumns) {
 			return "version properties";
-		} else {
-			return "";
 		}
+
+		throw new IllegalArgumentException("Unknown element type: " + element.getClass());
 	}
 }
