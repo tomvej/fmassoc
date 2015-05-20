9eccf2ff1a59a5312fa53b9217c3af790f57c5c6
Tomáš Vejpustek <tom.vejpustek@centrum.cz>
2015-05-20 13:15:27 +0200

    rm #5262 getTables implemented

--- org.tomvej.fmassoc.model/src/org/tomvej/fmassoc/model/path/Path.java
+++ org.tomvej.fmassoc.model/src/org/tomvej/fmassoc/model/path/Path.java
@@ -1,9 +1,11 @@
 package org.tomvej.fmassoc.model.path;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.tomvej.fmassoc.model.db.Association;
 import org.tomvej.fmassoc.model.db.AssociationProperty;
+import org.tomvej.fmassoc.model.db.Table;
 
 /**
  * Path comprised of linked atomic associations. May carry some properties.
@@ -16,4 +18,14 @@ public interface Path extends Association, PathInfo {
 	 * List of comprising associations (from source to destination).
 	 */
 	List<AssociationProperty> getAssociations();
+
+	/**
+	 * List of comprising tables (from source to destination).
+	 */
+	default List<Table> getTables() {
+		List<Table> result = getAssociations().stream().
+				map(a -> a.getSource()).collect(Collectors.toList());
+		result.add(getDestination());
+		return result;
+	}
 }
